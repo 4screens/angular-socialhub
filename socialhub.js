03d950ee3f92fc38a73e6e9f873c0de24a5ff4f9
Tomasz Pietrzak <tomasz.pietrzak@nopattern.net>
2014-10-15 14:01:36 +0200

    feat(dest): New build files.

--- socialhub.js
+++ socialhub.js
@@ -1,18 +1,68 @@
-/**
- * This file is subject to the terms and conditions defined in
- * file 'LICENSE' or 'LICENSE.txt', which is part of this source code package.
- *
- * Created by misiek on 14.10.2014.
- */
+/*
+ 4screens-socialhub v0.0.1
+ (c) 2014 Nopattern sp. z o.o.
+ License: proprietary
+*/
+'use strict';
+
+angular.module('4screens.socialhub',[]);
 
 'use strict';
 
-angular.module('4screens.socialhub', []).factory('socialhub',
-  function() {
+angular.module('4screens.socialhub').factory('SocialhubBackendService',
+  ["CONFIG", "socketService", "$http", "$q", function( CONFIG, socketService, $http, $q ) {
+    var
+      socket = socketService.get( CONFIG.socialhub.namespace + CONFIG.socialhub.id ),
+      config = {
+         post: CONFIG.backend.domain + CONFIG.socialhub.post.replace( ':id', CONFIG.socialhub.id ),
+         posts: CONFIG.backend.domain + CONFIG.socialhub.posts.replace( ':id', CONFIG.socialhub.id )
+      },
+      results = {};
+
+    function getPost( postId ) {
+      if( !postId ) {
+        throw 'PostId has not been set!';
+      }
+
+      return $http.get( config.post.replace( ':postid', postId ) ).then(function( res ) {
+        if ( res.status === 200 ) {
+          return res.data;
+        }
+        return $q.reject( res.data );
+      });
+    }
+
+    function getPosts( params ) {
+      params = params || {};
+
+      return $http.get( config.posts, params ).then(function( res ) {
+        if ( res.status === 200 ) {
+          return res.data;
+        }
+        return $q.reject( res.data );
+      });
+    }
+
+    socket.on( 'socialhub:newPost', function( postId ) {
+      getPost( postId ).then(function( post ) {
+        results.posts.unshift( post );
+      }).catch(function( err ) {
+        if( err.status === 404 ) {
+          _.remove( results.posts, { _id: postId } );
+        }
+      });
+    } );
+
     return {
-      hello: function() {
-        return 'hello';
+      results: results,
+      getLatest: function( page ) {
+        page = page || 0;
+
+        return getPosts({ 'page': page }).then(function( posts ) {
+          results.posts = ( results.posts || [] ).concat( posts );
+          return posts;
+        });
       }
     };
-  }
+  }]
 );
