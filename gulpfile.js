3ba12378624437281bb5710a4f4ab79f54a8b196
Tomasz Pietrzak <tomasz.pietrzak@nopattern.net>
2014-11-20 09:23:21 +0100

    contrib(gulp): Add html2js task with minifyhtml support.

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,12 @@ var gulp = require('gulp')
 , pkg = require('./package.json')
 , fs = require('fs')
 
+, VIEWS = [
+    './src/views/example.html'
+  ]
 , FILES = [
     './src/app.js',
+    './src/views.js',
     './src/directives/isotope.js',
     './src/directives/isotope-tile.js',
     './src/services/infinity.js',
@@ -18,24 +22,59 @@ var gulp = require('gulp')
 , BANNER = './src/header.txt'
 , MAIN = 'socialhub.js';
 
-gulp.task( 'build', function() {
+gulp.task( 'clean:tmp', function() {
+  return gulp.src( '.tmp', { read: false } )
+    .pipe( plugins.clean() );
+} );
+
+gulp.task( 'clean:views', ['copy'], function() {
+  return gulp.src( './src/views.js', { read: false } )
+    .pipe( plugins.clean() );
+} );
+
+gulp.task( 'minify:html', ['clean:tmp'], function() {
+  return gulp.src( VIEWS )
+    .pipe( plugins.minifyHtml({
+      empty: true,
+      quotes: true
+    }) )
+    .pipe( gulp.dest('.tmp/views') );
+} );
+
+gulp.task( 'html2js', ['minify:html'], function() {
+  return gulp.src( '.tmp/views/*.html' )
+    .pipe( plugins.ngHtml2js({
+        moduleName: pkg.name.replace( '-', '.' ),
+        declareModule: false,
+        prefix: 'views/%s/'.replace( '%s', pkg.name.split('-')[1] )
+    }) )
+    .pipe( gulp.dest('.tmp/views') );
+} );
+
+gulp.task( 'build:html', ['html2js'], function() {
+  return gulp.src('.tmp/views/*.js')
+    .pipe( plugins.concat( 'views.js' ) )
+    .pipe( gulp.dest('src') );
+} );
+
+gulp.task( 'build', ['build:html'], function() {
   return gulp.src( FILES )
     .pipe( plugins.concat( MAIN ) )
     .pipe( plugins.ngAnnotate() )
     .pipe( plugins.header( fs.readFileSync( BANNER, 'utf8' ), { pkg : pkg } ) )
-    .pipe( gulp.dest('.') )
+    .pipe( gulp.dest('.') );
 } );
 
-gulp.task( 'minify', function() {
+gulp.task( 'minify', ['build'], function() {
   return gulp.src( MAIN )
     .pipe( plugins.plumber() )
     .pipe( plugins.uglify() )
     .pipe( plugins.rename({ extname: '.min.js' }) )
     .pipe( plugins.header( fs.readFileSync( BANNER, 'utf8' ), { pkg : pkg } ) )
-    .pipe( gulp.dest('.') )
+    .pipe( gulp.dest('.') );
 } );
 
-gulp.task( 'copy', function() {
+gulp.task( 'copy', [ 'minify' ], function() {
   return gulp.src([ MAIN, MAIN.replace('.js', '.min.js') ])
     .pipe( gulp.dest('examples/client/vendor') );
 } );
@@ -46,7 +85,7 @@ gulp.task( 'complexity', function() {
 } );
 
 gulp.task( 'watch', function() {
-  gulp.watch( FILES, [ 'build', 'complexity', 'minify', 'copy'] );
+  gulp.watch( FILES, [ 'complexity', 'clean:views' ] );
 } );
 
-gulp.task( 'default', [ 'build', 'minify', 'copy' ] );
+gulp.task( 'default', ['clean:views'] );
