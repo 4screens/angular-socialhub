3e9d10be6aaaf5f3e113cdc55d86f8fe5f676aa3
Tomasz Pietrzak <tomasz.pietrzak@nopattern.net>
2014-11-27 15:00:49 +0100

    feat(scr): Add videogular poster plugin.

--- src/app.js
+++ src/app.js
@@ -4,5 +4,6 @@ angular.module('4screens.socialhub',[
   'com.2fdevs.videogular',
   'com.2fdevs.videogular.plugins.buffering',
   'com.2fdevs.videogular.plugins.controls',
-  'com.2fdevs.videogular.plugins.overlayplay'
+  'com.2fdevs.videogular.plugins.overlayplay',
+  'com.2fdevs.videogular.plugins.poster'
 ]);
