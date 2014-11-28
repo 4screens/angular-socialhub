0a5328760b252d2d16a7cd8275bc10295e8aae83
Tomasz Pietrzak <tomasz.pietrzak@nopattern.net>
2014-11-28 15:00:35 +0100

    fix(src): Replace jQuery function on to AngularJS.

--- src/services/infinity.js
+++ src/services/infinity.js
@@ -13,7 +13,7 @@ angular.module('4screens.socialhub').factory('SocialhubInfinityService',
 
     scrollHandler = _.throttle( function( s, e, w ) {
       return function() {
-        if( w.innerHeight - e.offset().top + w.scrollY + offset >= e.height() ) {
+        if( w.innerHeight - e.prop('offsetTop') + w.scrollY + offset >= parseInt( e.css('height'), 10 ) ) {
           if( !!available ) {
             available = false;
             SocialhubBackendService.renderVisibled( step );
