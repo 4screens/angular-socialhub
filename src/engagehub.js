76c3fffd0c9fac6b683348882c2b4b8ce238a71c
Marcin Marzewski <marcin.marzewski@nopattern.net>
2015-10-07 15:39:31 +0200

    chore(engagehub): Updated commerceUrl & text

--- src/engagehub.js
+++ src/engagehub.js
@@ -99,10 +99,9 @@ angular
         $rootScope.$emit('isotopeArrange');
       }
 
-      function changeCommerceUrl(shId, postId, url) {
-        console.debug('[ Engagehub Service ] ChangeCommerceUrl');
-        return $http.post(URL + CONFIG.backend.engagehub.postsModeration.replace(':id', shId).replace(':postId', postId),
-          {commerceUrl: url})
+      function changeCommerce(shId, postId, commerce) {
+        console.debug('[ Engagehub Service ] ChangeCommerce');
+        return $http.post(URL + CONFIG.backend.engagehub.postsModeration.replace(':id', shId).replace(':postId', postId), {commerce: commerce})
           .then(function(data) {
             return data.data;
           });
@@ -361,7 +360,7 @@ angular
         getHub: getHub,
         getPosts: getPosts,
         removePost: removePost,
-        changeCommerceUrl: changeCommerceUrl,
+        changeCommerce: changeCommerce,
         changeModeration: changeModeration,
         changeFeatured: changeFeatured,
         changePinned: changePinned,
