0cc18a22fdffd38d96e679c1c366144bef2f3dd7
Anna <anna.themasters@gmail.com>
2016-08-12 13:10:39 +0200

    fix replace of undefinded

--- src/view.js
+++ src/view.js
@@ -208,21 +208,25 @@ angular
         return imgUrl.replace('$$cloudinary$$', CONFIG.backend.cloudinary.domain).replace('$$bucket$$', CONFIG.backend.cloudinary.account);
       };
       $scope.linkify = function(inputText) {
-        var replacedText, replacePattern1, replacePattern2, replacePattern3;
+        if (inputText) {
+          var replacedText, replacePattern1, replacePattern2, replacePattern3;
 
-        //URLs starting with http://, https://, or ftp://
-        replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
-        replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+          //URLs starting with http://, https://, or ftp://
+          replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+          replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
 
-        //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
-        replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-        replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+          //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+          replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+          replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
 
-        //Change email addresses to mailto:: links.
-        replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
-        replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+          //Change email addresses to mailto:: links.
+          replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+          replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
 
-        return replacedText;
+          return replacedText;
+        } else {
+          return inputText;
+        }
       }
     }
   )
